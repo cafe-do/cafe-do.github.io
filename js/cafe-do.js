30d91ef9cfc55f274921aa5656fbdaa374dc771a
Diego Versiani <diegoversiani@gmail.com>
2016-03-22 20:22:56 -0300

    Added background video play/pause control.

---
+++ js/cafe-do.js
@@ -0,0 +1,42 @@
+// Closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+// Play or pause background video
+$("#jumbotron-video-control").click(function(e) {
+    var video = $("#jumbotron-video").get(0);
+
+    if (video.paused)
+        video.play();
+    else 
+        video.pause();
+
+    $("#jumbotron-video-control i")
+        .toggleClass('fa-pause')
+        .toggleClass('fa-play');
+});
